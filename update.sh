4ec6f632580b335de4abb867b621ef7c933b2f02
HackSavant <peter.ookoth@gmail.com>
2024-10-23 04:42:25 +0000

    adding a function to the script

--- update.sh
+++ update.sh
@@ -4,26 +4,24 @@ release_file=/etc/os-release
 logfile=/var/log/updater.log
 errorlog=/var/log/updater_errors.log
 
-if grep -q "Arch" $release_file
-then
-	# The host is based on Arch, run the pacman update command
-	sudo pacman -$yu 1>>$logfile 2>>$errorlog
+check_exit_status(){
 	if [ $? -ne 0 ]
 	then
 		echo "An error occurred, please check the $errorlog file."
 	fi
+}
+
+if grep -q "Arch" $release_file
+then
+	# The host is based on Arch, run the pacman update command
+	sudo pacman -$yu 1>>$logfile 2>>$errorlog
+	check_exit_status	
 fi
 
 if grep -q "Parrot Security" $release_file || grep -q "Ubuntu" $release_file
 then
 	 sudo apt update 1>>$logfile 2>>$errorlog
-	 if [ $? -ne 0 ]
-	 then
-	 	echo "An error occurred, please check the $errorlog file."
-	 fi
+	 check_exit_status
 	 sudo apt dist-upgrade -y 1>>$logfile 2>>$errorlog
-	 if [ $? -ne 0 ]
-	 then
-	 	echo "An error occurred, please check the $errorlog file."
-	 fi
+	 check_exit_status
 fi
